6fe5c49ee485331e21876ea6eb0997cf7e807f17
ananya-git22 <anu22human@gmail.com>
2024-10-26 20:18:09 +0000

    implemented few math functions

--- src/script.js
+++ src/script.js
@@ -1,50 +1,64 @@
-<script>
-        document.addEventListener('DOMContentLoaded', function() {
+
+
+    document.addEventListener('DOMContentLoaded', function() {
     // Your code here
-    console.log('Document is ready!');
-    const display = document.getElementById('calc-display');
-    const buttons = document.getElementsByClassName('btn');
+      console.log('Document is ready!')
+       const display = document.getElementById('calc-display');
+       const buttons = document.getElementsByClassName('btn');
 
-    let currentvalue = "";
+      let currentvalue = "";
 
-    function evaluateResult(){
-        console.log('currentvalue:', currentvalue)
-        const convertedvalue = currentvalue
-           .replace("×","*")
-           .replace("÷","/")
-           .replace('%','*0.01')
-        console.log('convertedvalue:', convertedvalue)
-        const result = eval(convertedvalue);
-        currentvalue = result.toString();
-        display.value = currentvalue;
-    }
+      function evaluateResult(){
+          console.log('currentvalue:', currentvalue)
+          const convertedvalue = currentvalue
+             .replace("×","*")
+             .replace("÷","/")
+             .replace('%','*0.01')
+             .replace('sin','Math.sin')
+             .replace('cos','Math.cos')
+             .replace('ln','Math.log')
+             .replace('π','Math.pi')
+             .replace('log','Math.log10')
+             .replace('e','Math.E')
+             .replace('tan','Math.tan')
+             .replace('√','Math.sqrt');
 
-    for(let i=0; i< buttons.length; i++){
-        const button = buttons[i];
-        button.addEventListener('click',function() {
-            const value =  button.innerText;
+          console.log('convertedvalue:', convertedvalue)
+          const result = eval(convertedvalue);
+          currentvalue = result.toString();
+          display.value = currentvalue;
+        }
 
+        for(let i=0; i< buttons.length; i++){
+            const button = buttons[i];
+            button.addEventListener('click',function() {
+               const value =  button.innerText;
 
-            if(value == "AC"){
-                currentvalue = "";
+               try{
+                if(value == "AC"){
+                    currentvalue = "";
+                    display.value = currentvalue;
+                  } else if(value == "=") {
+                    evaluateResult()
+                  } else {
+                    currentvalue += value;
+                    console.log('currentvalue:', currentvalue)
+                    display.value = currentvalue;
+                  }
+
+               } catch(error) {
+                console.error(error);
+                currentvalue = "ERROR"
                 display.value = currentvalue;
-            } else if(value == "="){
-                evaluateResult()
-            }
-            
-            
-            else{
-                currentvalue += value;
-                console.log('currentvalue:', currentvalue)
-                display.value = currentvalue;
-            }
-
-
-
-        })
-    }
+               }
+            })
+        }
+        
+        
+    
     
 
     });
 
-    </script>
+   
+   
